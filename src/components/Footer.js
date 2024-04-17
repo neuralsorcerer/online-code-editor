b9265c50e109e057e2b41670faa8363ca989d9f3
Soumyadip Sarkar <soumyadip@soumyadipsarkar.in>
2024-04-17 23:59:46 +0530

    update

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="fixed h-8 bottom-0 px-2 py-1 flex items-center justify-center w-full text-xs text-white text-center">
+      <span>
+        Built with{" "}
+        ❤️{" "}
+      </span>
+    </div>
+  );
+};
+
+export default Footer;
